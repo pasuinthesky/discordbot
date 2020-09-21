2effe6133052169a9e6aa03d1ef455c034e99425
Glen Liu <glen.liu@cautiontape.ca>
2020-09-21 19:42:09 +0000

    VS-2 this is a test.

--- botctl.sh
+++ botctl.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/bash
 #
 
+function bot_running() {
+    ps -ef|grep -v grep |grep "python3 Bot.py"
+    return $?
+}
+
 function start_bot() {
-    source ~/bin/activate
-    python3 Bot.py 1>Bot.log 2>&1 &
+    if bot_running; then
+        echo "Bot has already been running. exit..."
+    else
+        echo "Starting bot..."
+        source ~/bin/activate
+        python3 Bot.py 1>Bot.log 2>&1 &
+        echo "Bot started..."
+    fi
 }
 
 function stop_bot() {
-    while ps -ef|grep -v grep |grep "python3 Bot.py"
+    while bot_running
     do
-        ps -ef|grep -v grep |grep Bot.py | xargs kill -15
-        echo "Kill signal sent. Wating for Bot to exit...."
+        ps -ef|grep -v grep |grep Bot.py | awk '{print $2}' | xargs kill -15
+        echo "Kill signal sent. Wating for bot to exit...."
         date
         echo
         sleep 5
     done
+    echo "Bot stopped..."
 }
 
 case "$1" in
@@ -31,4 +43,3 @@ restart)
 *)
     echo "Usage: botctl [start|stop|restart]"
 esac
-    
